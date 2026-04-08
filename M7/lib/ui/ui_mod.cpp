b63abe0096fc1ebc9ae82e7248312c714e884161
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-04-08 08:32:05 +0200

    added label bind text by using subjects structure

--- M7/lib/ui/ui_mod.cpp
+++ M7/lib/ui/ui_mod.cpp
@@ -4,8 +4,22 @@
 
 extern Arduino_H7_Video Display;
 
+static lv_subject_t s_temp;
+static lv_subject_t s_hum;
+static lv_subject_t s_press;
+static lv_subject_t s_dev_name;
+static char device_name_buffer[32] = "--";
+
+void init_data_subjects(){
+    lv_subject_init_int(&s_temp, INT32_MIN);
+    lv_subject_init_int(&s_hum, INT32_MIN);
+    lv_subject_init_int(&s_press, INT32_MIN);
+    lv_subject_init_pointer(&s_dev_name, device_name_buffer);
+};
 
 void initUI(){
+
+    init_data_subjects();
 
     //ESS Screen
     lv_obj_t * screen = lv_obj_create(lv_scr_act());
@@ -17,25 +31,26 @@ void initUI(){
 
     //label sourcedevice + MAC
     auto label_device_name = lv_label_create(flexbox);
-    lv_label_set_text_fmt(label_device_name, "Device : %s (%s)", "--", "--:--:--:--:--:--");
     lv_obj_set_width(label_device_name, lv_pct(100));
     lv_obj_set_style_text_font(label_device_name, &lv_font_montserrat_48, 0);
+    lv_label_bind_text(label_device_name,&s_dev_name,"Device : %s");
 
     //label temperature +  data
     auto label_temperature = lv_label_create(flexbox);
-    lv_label_set_text_fmt(label_temperature, "Temperature : %s", "--");
     lv_obj_set_width(label_temperature, lv_pct(100));
     lv_obj_set_style_text_font(label_temperature, &lv_font_montserrat_30, 0);
+    lv_label_bind_text(label_temperature,&s_temp,"Temperature : %d C");
     
     //label humidity +  data
     auto label_humidity = lv_label_create(flexbox);
-    lv_label_set_text_fmt(label_humidity, "Humidity : %s", "--");
     lv_obj_set_width(label_humidity, lv_pct(100));
     lv_obj_set_style_text_font(label_humidity, &lv_font_montserrat_30, 0);
+    lv_label_bind_text(label_humidity,&s_hum,"Humidity : %d %%");
+
     //label pressure +  data
     auto label_pressure = lv_label_create(flexbox);
-    lv_label_set_text_fmt(label_pressure, "Pressure : %s", "--");
     lv_obj_set_width(label_pressure, lv_pct(100));
     lv_obj_set_style_text_font(label_pressure, &lv_font_montserrat_30, 0);
+    lv_label_bind_text(label_pressure,&s_press,"Pressure : %d hPa");
 
 };
