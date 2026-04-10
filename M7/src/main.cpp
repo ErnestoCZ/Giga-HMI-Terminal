ef8de2c703e31329edc96e2a58d3271ad2732ba3
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-04-10 07:37:21 +0200

    added basic capabilities to connect to nrf5340 sensor node via BLE : currently mostly hardcoded service and devicenames

--- M7/src/main.cpp
+++ M7/src/main.cpp
@@ -6,13 +6,18 @@
 #include "ui_mod.h"
 Arduino_H7_Video Display(DISPLAY_WIDTH,DISPLAY_HEIGHT,GigaDisplayShield);
 Arduino_GigaDisplayTouch TouchDetector;
-
-
+#include <ArduinoBLE.h>
+BLEDevice peripheral;
 void setup() {
   // put your setup code here, to run once:
   if(Display.begin() != 0) return;
   if(TouchDetector.begin() == false) return;
-  
+
+  BLE.begin();
+
+  Serial.println("Init completed");
+  BLE.scanForName("NRF5340Dev");
+
 
   //Creation of lvgl graphics
   initUI();
@@ -22,4 +27,46 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   lv_timer_handler();
+  peripheral = BLE.available();
+  if(peripheral){
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    ui_set_device_name(peripheral.localName().c_str(), (size_t)peripheral.localName().length());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+    peripheral.connect();
+    if(peripheral.connected()){
+      peripheral.discoverService(peripheral.advertisedServiceUuid().c_str());
+      if(peripheral.characteristicCount() > 0){
+        BLEService service = peripheral.service(peripheral.advertisedServiceUuid().c_str());
+        Serial.println("Found the following characteristics in ESS:");
+        for (int i = 0; i < service.characteristicCount(); i++)
+        {
+          int32_t charValue = 0;
+          BLECharacteristic characteristic = peripheral.characteristic(i);
+          Serial.println(characteristic.uuid());
+          if(characteristic.read()){
+            characteristic.readValue(charValue);
+            if(strcmp(characteristic.uuid(), "2a6e") == 0){
+              ui_set_temperature_value(charValue);
+            }
+            if(strcmp(characteristic.uuid(), "2a6f") == 0){
+              ui_set_humidity_value(charValue);
+            }
+            if(strcmp(characteristic.uuid(), "2a6d") == 0){
+              ui_set_pressure_value(charValue);
+            }
+          }
+        }
+        
+      }
+      
+        
+      peripheral.disconnect();
+      BLE.scanForName("NRF5340Dev");
+    }
+  }
 }
