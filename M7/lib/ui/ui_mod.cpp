90675c6197347d21cf0ff8de9d55718f8ddba5a5
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-04-11 10:58:34 +0200

    cleaned up code

--- M7/lib/ui/ui_mod.cpp
+++ M7/lib/ui/ui_mod.cpp
@@ -1,76 +1,16 @@
 #include "ui_mod.h"
 #include <lvgl.h>
 #include <Arduino_H7_Video.h>
+#include "components/tw_tab_ess.h"
 
 extern Arduino_H7_Video Display;
 
-#define DEVICE_NAME_BUFFER_LEN 32
-static lv_subject_t s_temp;
-static lv_subject_t s_hum;
-static lv_subject_t s_press;
-static lv_subject_t s_dev_name;
-static char device_name_buffer[DEVICE_NAME_BUFFER_LEN] = "--";
-
-void init_data_subjects(){
-    lv_subject_init_int(&s_temp, 0);
-    lv_subject_init_int(&s_hum, 0);
-    lv_subject_init_int(&s_press, 0);
-    lv_subject_init_pointer(&s_dev_name, device_name_buffer);
-};
-
-int ui_set_device_name(const char * dev_name, const unsigned int len){
-    if(len > DEVICE_NAME_BUFFER_LEN) return 1;
-
-    strcpy(device_name_buffer,dev_name);
-    lv_subject_set_pointer(&s_dev_name,device_name_buffer);
-    return 0;
-};
-int ui_set_temperature_value(const int32_t val){
-    lv_subject_set_int(&s_temp, val);
-    return 0;
-};
-int ui_set_humidity_value(const int32_t val){
-    lv_subject_set_int(&s_hum,val);
-    return 0;
-};
-int ui_set_pressure_value(const int32_t val){
-    lv_subject_set_int(&s_press, val);
-    return 0;
-};
-
 void initUI(){
 
-    init_data_subjects();
-
-    //ESS Screen
-    lv_obj_t * screen = lv_obj_create(lv_scr_act());
+    lv_obj_t * screen = lv_screen_active();
     lv_obj_set_size(screen, Display.width(),Display.height());
 
-    lv_obj_t * flexbox = lv_obj_create(screen);
-    lv_obj_set_flex_flow(flexbox, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_size(flexbox, lv_pct(100), lv_pct(100));
+    auto tabWidget = lv_tabview_create(screen);
+    create_tw_tab_ess(tabWidget);
 
-    //label sourcedevice + MAC
-    auto label_device_name = lv_label_create(flexbox);
-    lv_obj_set_width(label_device_name, lv_pct(100));
-    lv_obj_set_style_text_font(label_device_name, &lv_font_montserrat_48, 0);
-    lv_label_bind_text(label_device_name,&s_dev_name,"Device : %s");
-
-    //label temperature +  data
-    auto label_temperature = lv_label_create(flexbox);
-    lv_obj_set_width(label_temperature, lv_pct(100));
-    lv_obj_set_style_text_font(label_temperature, &lv_font_montserrat_30, 0);
-    lv_label_bind_text(label_temperature,&s_temp,"Temperature : %d C");
-    
-    //label humidity +  data
-    auto label_humidity = lv_label_create(flexbox);
-    lv_obj_set_width(label_humidity, lv_pct(100));
-    lv_obj_set_style_text_font(label_humidity, &lv_font_montserrat_30, 0);
-    lv_label_bind_text(label_humidity,&s_hum,"Humidity : %d %%");
-
-    //label pressure +  data
-    auto label_pressure = lv_label_create(flexbox);
-    lv_obj_set_width(label_pressure, lv_pct(100));
-    lv_obj_set_style_text_font(label_pressure, &lv_font_montserrat_30, 0);
-    lv_label_bind_text(label_pressure,&s_press,"Pressure : %d hPa");
 };
--- M7/lib/ui/ui_mod.h
+++ M7/lib/ui/ui_mod.h
@@ -2,7 +2,3 @@
 #include <Arduino.h>
 
 void initUI(void);
-int ui_set_device_name(const char * dev_name, const size_t len);
-int ui_set_temperature_value(const int32_t val);
-int ui_set_humidity_value(const int32_t val);
-int ui_set_pressure_value(const int32_t val);
