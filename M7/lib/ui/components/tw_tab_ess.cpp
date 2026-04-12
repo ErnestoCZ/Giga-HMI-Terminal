d014aeefd5f5770525cd6a4580a0a484c2e62a44
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-04-12 14:14:49 +0200

    removed flexbox and added widgets directly to tab

--- M7/lib/ui/components/tw_tab_ess.cpp
+++ M7/lib/ui/components/tw_tab_ess.cpp
@@ -42,33 +42,34 @@ void create_tw_tab_ess(lv_obj_t* parent){
     init_data_subjects();
     auto tab = lv_tabview_add_tab(parent, "ESS");
 
-    lv_obj_t * flexbox = lv_obj_create(tab);
-    lv_obj_set_flex_flow(flexbox, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_size(flexbox, lv_pct(100), lv_pct(100));
-    lv_obj_set_scrollbar_mode(flexbox,LV_SCROLLBAR_MODE_AUTO);
+    lv_obj_set_flex_flow(tab, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_size(tab, lv_pct(100), lv_pct(100));
+    lv_obj_set_scrollbar_mode(tab,LV_SCROLLBAR_MODE_AUTO);
 
     //label sourcedevice + MAC
-    auto label_device_name = lv_label_create(flexbox);
+    auto label_device_name = lv_label_create(tab);
     lv_obj_set_width(label_device_name, lv_pct(100));
     lv_obj_set_style_text_font(label_device_name, &lv_font_montserrat_48, 0);
     lv_label_bind_text(label_device_name,&s_dev_name,"Device : %s");
 
     //label temperature +  data
-    auto label_temperature = lv_label_create(flexbox);
+    auto label_temperature = lv_label_create(tab);
     lv_obj_set_width(label_temperature, lv_pct(100));
     lv_obj_set_style_text_font(label_temperature, &lv_font_montserrat_30, 0);
     lv_label_bind_text(label_temperature,&s_temp,"Temperature : %d C");
     
     //label humidity +  data
-    auto label_humidity = lv_label_create(flexbox);
+    auto label_humidity = lv_label_create(tab);
     lv_obj_set_width(label_humidity, lv_pct(100));
     lv_obj_set_style_text_font(label_humidity, &lv_font_montserrat_30, 0);
     lv_label_bind_text(label_humidity,&s_hum,"Humidity : %d %%");
 
     //label pressure +  data
-    auto label_pressure = lv_label_create(flexbox);
+    auto label_pressure = lv_label_create(tab);
     lv_obj_set_width(label_pressure, lv_pct(100));
     lv_obj_set_style_text_font(label_pressure, &lv_font_montserrat_30, 0);
     lv_label_bind_text(label_pressure,&s_press,"Pressure : %d hPa");
+
+    auto chart = lv_chart_create(tab);
 
 };
