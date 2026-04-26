e933b80a8934f6a867b7e75c85ca7618679cc6c5
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-04-26 20:03:20 +0200

    added change display backlight support in Settings Tab

--- M7/lib/ui/components/tw_tab_settings.cpp
+++ M7/lib/ui/components/tw_tab_settings.cpp
@@ -1,8 +1,17 @@
 #include "tw_tab_settings.h"
+#include <Arduino_GigaDisplay.h>
 
+constexpr int32_t DISPLAY_BRIGHTNESS_MIN = 20; 
+
+
+static GigaDisplayBacklight backlight;
 static lv_subject_t s_display_brightness;
 
+//PFD
+void on_brightness_change(lv_observer_t * observer, lv_subject_t * subject);
+
 static void init_subjects(){
+    backlight.begin();
     lv_subject_init_int(&s_display_brightness, 100);
 };
 
@@ -22,9 +31,16 @@ void create_tw_tab_settings(lv_obj_t* parent){
     lv_obj_set_size(tab,lv_pct(100),lv_pct(100));
     lv_obj_set_scrollbar_mode(tab,LV_SCROLLBAR_MODE_AUTO);
 
-    auto box = createSettingBox(tab);
-    auto display_brightness_label = lv_label_create(box);
+    auto box_displayBrightness = createSettingBox(tab);
+    auto display_brightness_label = lv_label_create(box_displayBrightness);
     lv_label_bind_text(display_brightness_label,&s_display_brightness,"Display Brightness (%d):");
-    auto display_brightness_slider = lv_slider_create(box);
+    auto display_brightness_slider = lv_slider_create(box_displayBrightness);
+    lv_slider_set_range(display_brightness_slider,DISPLAY_BRIGHTNESS_MIN,100);
     lv_slider_bind_value(display_brightness_slider,&s_display_brightness);
+    lv_subject_add_observer(&s_display_brightness,on_brightness_change,NULL);
+};
+
+void on_brightness_change(lv_observer_t * observer, lv_subject_t * subject){
+    int32_t value = lv_subject_get_int(subject);
+    backlight.set(value);
 };
