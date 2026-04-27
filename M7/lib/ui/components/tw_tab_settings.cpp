fb8e3920d9ec7d4a026bb9146aff91b8220f81da
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-04-27 08:45:48 +0200

    added init wifi settings option : without functionality

--- M7/lib/ui/components/tw_tab_settings.cpp
+++ M7/lib/ui/components/tw_tab_settings.cpp
@@ -17,9 +17,10 @@ static void init_subjects(){
 
 static lv_obj_t* createSettingBox(lv_obj_t* parent){
     auto box = lv_obj_create(parent);
-    lv_obj_set_size(box, lv_pct(100), lv_pct(20));
+    lv_obj_set_size(box, lv_pct(100), lv_pct(25));
     lv_obj_set_flex_flow(box,LV_FLEX_FLOW_ROW);
     lv_obj_set_flex_align(box, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+    lv_obj_clear_flag(box,LV_OBJ_FLAG_SCROLLABLE);
     return box;
 };
 
@@ -31,6 +32,23 @@ void create_tw_tab_settings(lv_obj_t* parent){
     lv_obj_set_size(tab,lv_pct(100),lv_pct(100));
     lv_obj_set_scrollbar_mode(tab,LV_SCROLLBAR_MODE_AUTO);
 
+
+    //WiFi Networks
+    auto box_WiFiNetworks = createSettingBox(tab);
+    lv_obj_set_scrollbar_mode(box_WiFiNetworks,LV_SCROLLBAR_MODE_OFF);
+    auto wifi_networks_label = lv_label_create(box_WiFiNetworks);
+    lv_label_set_text_fmt(wifi_networks_label,"WiFi Networks : ");
+    auto wifi_networks_dropdown = lv_dropdown_create(box_WiFiNetworks);
+    lv_obj_set_flex_grow(wifi_networks_dropdown,2);
+    lv_dropdown_set_options(wifi_networks_dropdown,"");
+    auto wifi_networks_button_search = lv_button_create(box_WiFiNetworks);
+    auto wifi_networks_button_search_label = lv_label_create(wifi_networks_button_search);
+    lv_label_set_text_fmt(wifi_networks_button_search_label,"search");
+    auto wifi_networks_button_connect = lv_button_create(box_WiFiNetworks);
+    auto wifi_networks_button_connect_label = lv_label_create(wifi_networks_button_connect);
+    lv_label_set_text_fmt(wifi_networks_button_connect_label,"Connect");
+
+    //DISPLAY Brightness
     auto box_displayBrightness = createSettingBox(tab);
     auto display_brightness_label = lv_label_create(box_displayBrightness);
     lv_label_bind_text(display_brightness_label,&s_display_brightness,"Display Brightness (%d):");
