0dbe494b2f32cd74cb3ef59e35dd7bae4b0726fb
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-05-29 07:12:38 +0200

    tabwidget cleared flag scrollable (disabled gesture support)

--- M7/lib/ui/ui_mod.cpp
+++ M7/lib/ui/ui_mod.cpp
@@ -12,6 +12,8 @@ void initUI(){
     lv_obj_set_size(screen, Display.width(),Display.height());
 
     auto tabWidget = lv_tabview_create(screen);
+    auto tabContent = lv_tabview_get_content(tabWidget);
+    lv_obj_clear_flag(tabContent,LV_OBJ_FLAG_SCROLLABLE); //Turns of gesture tab switch
     create_tw_tab_ess(tabWidget);
     create_tw_tab_settings(tabWidget);
 
