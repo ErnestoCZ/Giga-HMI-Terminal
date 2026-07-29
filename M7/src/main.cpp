ba18d4b1a44e5782e02b07b933c13e2845fedf4b
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-07-29 21:42:07 +0200

    implemented state machine

--- M7/src/main.cpp
+++ M7/src/main.cpp
@@ -10,6 +10,17 @@
 #include "LittleFileSystem.h"
 #include <Thread.h>
 
+enum class state{
+  INIT,
+  IDLE,
+  SCAN,
+  CONNECT,
+  SUBSCRIBE,
+};
+void stateMachine(enum state &currentState);
+
+static state currentState = state::INIT;
+
 rtos::Thread ui_thread(osPriorityNormal, 8192);
 rtos::Mutex lvgl_mutex;
 static void ui_thread_handler(){
@@ -25,26 +36,31 @@ QSPIFBlockDevice bd;
 mbed::LittleFileSystem fs("fs");
 
 
-#define BLE_ESS_UUID 181a
+#define BLE_ESS_UUID "181a"
 #define BLE_TEMPERATURE_CHAR "2a6e"
 #define BLE_HUMIDITY_CHAR "2a6f"
 #define BLE_PRESSURE_CHAR "2a6d"
-void BLE_TemperatureUpdateHandler(BLEDevice device, BLECharacteristic characteristic){
-  int32_t value = 0;
-  characteristic.readValue(value);
-  tw_ess_set_temperature_value(value);
-};
-void BLE_HumidityUpdateHandler(BLEDevice device, BLECharacteristic characteristic){
-  int32_t value = 0;
-  characteristic.readValue(value);
-  tw_ess_set_humidity_value(value);
 
-};
-void BLE_AirPressureUpdateHandler(BLEDevice device, BLECharacteristic characteristic){
+void BLE_ESS_TempHandler(BLEDevice device, BLECharacteristic characteristic){
   int32_t value = 0;
   characteristic.readValue(value);
-  tw_ess_set_pressure_value(value);
-
+  if(!strcmp(characteristic.uuid(), BLE_TEMPERATURE_CHAR)){
+    tw_ess_set_temperature_value(value);
+  }
+};
+void BLE_ESS_HumHandler(BLEDevice device, BLECharacteristic characteristic){
+  int32_t value = 0;
+  characteristic.readValue(value);
+  if(!strcmp(characteristic.uuid(), BLE_HUMIDITY_CHAR)){
+    tw_ess_set_humidity_value(value);
+  }
+};
+void BLE_ESS_PresHandler(BLEDevice device, BLECharacteristic characteristic){
+  int32_t value = 0;
+  characteristic.readValue(value);
+  if(!strcmp(characteristic.uuid(), BLE_PRESSURE_CHAR)){
+    tw_ess_set_pressure_value(value);
+  }
 };
 
 Arduino_H7_Video Display(DISPLAY_WIDTH,DISPLAY_HEIGHT,GigaDisplayShield);
@@ -55,64 +71,102 @@ void setup() {
   if(Display.begin() != 0) return;
   if(TouchDetector.begin() == false) return;
   ui_thread.start(ui_thread_handler);
+  Serial.begin(115200);
   BLE.begin();
-
-  int err = fs.mount(&bd);
-  if(err){
-    Serial.println("Mount failed. Formatting...");
-    err = fs.reformat(&bd);
-    if(err){
-      Serial.println("Error formatting QSPI Flash!");
-      return;
-    }
-  }
-  FILE *f =fopen("/fs/test.txt","r");
-  if(f == NULL){
-    Serial.println("Requested File does not exists");
-  }else{
-    char buffer[256];
-    while(fgets(buffer,sizeof(buffer),f)){
-      Serial.println(buffer);
-    }
-  }
 }
 
+static void checkAndSubscribe(const char* uuid, BLECharacteristicEventHandler handler){
+  Serial.print("Checking Characteristic ");
+  Serial.println(uuid);
+
+  if(peripheral.characteristic(uuid).canSubscribe()){
+    peripheral.characteristic(uuid).setEventHandler(BLEWritten, handler);
+    peripheral.characteristic(uuid).subscribe();
+  }
+};
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  BLE.scanForUuid("181a");
 
+  stateMachine(currentState);
+
+}
+
+
+void InitHandler(){
+
+};
+void IdleHandler(enum state &currentState){
+  if(!BLE.connected()){
+    //TODO resetUI()
+    currentState = state::SCAN;
+  }else{
+    currentState = state::IDLE;
+  }
+};
+void ScanHandler(enum state &currentState){
+  BLE.scanForUuid(BLE_ESS_UUID);
   peripheral = BLE.available();
-  if(peripheral && !peripheral.connected()){
-    BLE.stopScan();
-    Serial.println(peripheral.localName());
-    tw_ess_set_device_name(peripheral.localName().c_str(), peripheral.localName().length());
-    for (int i = 0; i < peripheral.advertisedServiceUuidCount(); i++)
-    {
-      Serial.println(peripheral.advertisedServiceUuid(i));
-      if(peripheral.advertisedServiceUuid(i) == "181a"){
+  if(!peripheral.connected()){
+    currentState = state::CONNECT;
+  }else{
+    currentState = state::SCAN;
+  };
 
-        if(!peripheral.connected()){
-          peripheral.connect();
-          Serial.println("Connection established to peripheral");
+};
+void ConnectHandler(enum state &currentState){
+  peripheral.connect();
+  if(peripheral.connected()){
+    auto remoteName = peripheral.localName();
+    tw_ess_set_device_name(remoteName.c_str(), remoteName.length());
+    currentState = state::SUBSCRIBE;
+  }else{
+    currentState = state::SCAN;
+  };
 
-        }else{
-          Serial.println("Peripheral already connected");
-        }
-        peripheral.discoverService("181a");
-        if(peripheral.hasCharacteristic(BLE_TEMPERATURE_CHAR) && peripheral.characteristic(BLE_TEMPERATURE_CHAR).canSubscribe()){
-          peripheral.characteristic(BLE_TEMPERATURE_CHAR).setEventHandler(BLEWritten, BLE_TemperatureUpdateHandler);
-          peripheral.characteristic(BLE_TEMPERATURE_CHAR).subscribe();
-        }
-        if(peripheral.hasCharacteristic(BLE_HUMIDITY_CHAR) && peripheral.characteristic(BLE_HUMIDITY_CHAR).canSubscribe()){
-          peripheral.characteristic(BLE_HUMIDITY_CHAR).setEventHandler(BLEWritten, BLE_HumidityUpdateHandler);
-          peripheral.characteristic(BLE_HUMIDITY_CHAR).subscribe();
-        }
-        if(peripheral.hasCharacteristic(BLE_PRESSURE_CHAR) && peripheral.characteristic(BLE_PRESSURE_CHAR).canSubscribe()){
-          peripheral.characteristic(BLE_PRESSURE_CHAR).setEventHandler(BLEWritten, BLE_AirPressureUpdateHandler);
-          peripheral.characteristic(BLE_PRESSURE_CHAR).subscribe();
-        }
+};
+void SubscribeHandler(enum state &currentState){
+
+  if (peripheral.discoverService(BLE_ESS_UUID)) {
+    // Explicitly check after discovering attributes
+    for (int i = 0; i < peripheral.characteristicCount(); i++) {
+      BLECharacteristic c = peripheral.characteristic(i);
+      
+      if (!strcmp(c.uuid(), BLE_TEMPERATURE_CHAR)) {
+        checkAndSubscribe(BLE_TEMPERATURE_CHAR, BLE_ESS_TempHandler);
+      } else if (!strcmp(c.uuid(), BLE_HUMIDITY_CHAR)) {
+        checkAndSubscribe(BLE_HUMIDITY_CHAR, BLE_ESS_HumHandler);
+      } else if (!strcmp(c.uuid(), BLE_PRESSURE_CHAR)) {
+        checkAndSubscribe(BLE_PRESSURE_CHAR, BLE_ESS_PresHandler);
       }
     }
   }
+  currentState = state::IDLE;
+};
 
+void stateMachine(enum state &currentState){
+  Serial.print("Current State is ");
+  Serial.println(static_cast<int>(currentState));
+
+  switch (currentState)
+  {
+  case state::INIT:
+    currentState = state::IDLE;
+    break;
+  case state::IDLE:
+    IdleHandler(currentState);
+    break;
+  case state::SCAN:
+    ScanHandler(currentState);
+    break;
+  case state::CONNECT:
+    ConnectHandler(currentState);
+    break;
+  case state::SUBSCRIBE:
+    SubscribeHandler(currentState);
+    break;
+  
+  default:
+    currentState = state::IDLE;
+    break;
+  }
 }
